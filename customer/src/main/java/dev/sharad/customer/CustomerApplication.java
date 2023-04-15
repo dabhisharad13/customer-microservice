44ebe25b5638dfb919566c20178af38d1cf7a0b8
dabhisharad13 <dabhisharad13@gmail.com>
2023-04-15 12:07:08 -0700

    Added gitignore, created application yml

--- customer/src/main/java/dev/sharad/customer/CustomerApplication.java
+++ customer/src/main/java/dev/sharad/customer/CustomerApplication.java
@@ -1,7 +1,12 @@
 package dev.sharad.customer;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CustomerApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        SpringApplication.run(CustomerApplication.class, args);
     }
 }
