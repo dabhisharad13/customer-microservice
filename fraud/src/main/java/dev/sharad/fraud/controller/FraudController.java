a6390ed3044c8a97a55478c31e7f5c9083443856
dabhisharad13 <dabhisharad13@gmail.com>
2023-04-18 11:41:53 -0700

    Fixed imports in Fraud Controller

--- fraud/src/main/java/dev/sharad/fraud/controller/FraudController.java
+++ fraud/src/main/java/dev/sharad/fraud/controller/FraudController.java
@@ -4,8 +4,10 @@ import dev.sharad.fraud.FraudCheckResponse;
 import dev.sharad.fraud.service.FraudCheckService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
+
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
 
